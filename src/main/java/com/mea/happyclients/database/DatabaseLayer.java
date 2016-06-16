c45006c36b31cbf981b6350340b07b2438649d0d
mark-micallef <mmicallef@gmail.com>
2016-06-17 01:36:08 +0200

    Working on Profile editing

--- src/main/java/com/mea/happyclients/database/DatabaseLayer.java
+++ src/main/java/com/mea/happyclients/database/DatabaseLayer.java
@@ -153,7 +153,6 @@ public class DatabaseLayer {
                 user.setId(rs.getString("id"));
                 user.setName(rs.getString("name"));
             }
-            result = rs.getInt(1) > 0;
         } catch (Exception e) {
             e.printStackTrace();
         }
